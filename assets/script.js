703ee18dc7a9c15781e1990f441d52c389bc068d
Ben Larocque <benlaro0214@hotmail.com>
2021-01-29 11:21:57 -0500

    solved the interface issue. Now questions are showing

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,23 @@
   // Set Global var based on JavaScript Best Practices.
   
   var countDownT
-  var countdownvVal 
+  var countdownU 
   var questionNum
   
   //this is where the questions begin. you can add more by copy and pasting the code for the questions.
   var questions = [
 // Question 1
-{ question: "question goes here", 
-answers: [ "answer1", "answer2", "answer3" ],
-correct: "Answer1" },
+{ question: "What is the Capital of Canada?", 
+answers: [ "Ottawa", "Tokyo", "Washington D.C." ],
+correct: "Ottawa" },
 // Question 2
-{ question: "question goes here", 
-answers: [ "answer4", "answer5", "answer6" ],
-correct: "answer6" },
+{ question: "Why is the sky blue?", 
+answers: [ "If it was green we wouldn't know when to stop mowing", "God did it.", "It is due to the way visible light refracts on the composition of the atmosphere" ],
+correct: "It is due to the way visible light refracts on the composition of the atmosphere" },
 // Qestion 3
-{ question: "question goes here", 
-answers: [ "answer7", "answer8", "answer9" ],
-correct: "answer8" }
+{ question: "What is the number that is the answer to life, the universe and everything", 
+answers: [ "99", "42", "over 9000" ],
+correct: "42" }
 ]
 
 
@@ -27,28 +27,28 @@ correct: "answer8" }
 
 function timerDandD( byValue=1 ){
   // decrease by the value passed in, or if nothing, by 1
-  countdownVal -= byValue
-  document.querySelector('#countdown').textContent = countdownVal
-  if( countdownVal<1 )
+  countdownValue -= byValue
+  document.querySelector('#countdown').textContent = countdownValue
+  if( countdownValue<1 )
     finishLine()
 }
 
 function startQuiz(){
   questionNum = 0  //Which question is it starting from?
-  countdownVal = 60 // How much time are you giving people to take this quiz? This is measured in seconds (i.e. 60 = one minute). 
-  countDownT = setInterval( timerDandD, 1000 ) //interval between seconds. 
+  countdownValue = 60 // How much time are you giving people to take this quiz? This is measured in seconds (i.e. 60 = one minute). 
+  countdownTimer = setInterval( timerDandD, 1000 ) //interval between seconds. 
   // switch back to the quizPage
- // showPage( 'quizPage' )
-  //showNextQuestion()
+ showPage( 'quizPage')
+  showNextQuestion()
 }
 startQuiz()
 
 function clockStop(event) {
   if( event ) event.preventDefault()
  //  stop the countdown
-  clearInterval( countdownT )
+  clearInterval( countdownTimer )
 //   show score page
-  showPage( 'scorePage' )
+  finishLine( 'scorePage' )
   }
   
   
@@ -94,14 +94,15 @@ function selectAnswer( event,answer ){
 function showPage( page ){
   // hide all pages
   document.querySelector('#quizPage').classList.add('d-none')
-  document.querySelector('#scorePage').classList.add('d-none')
+  //document.querySelector('#scorePage').classList.add('d-none')
   // show selected page
   document.querySelector(`#${page}`).classList.remove('d-none')
 }
 
 
 //=============THE FINISH LINE - How it will end (Scoreboard and reset the game==================//
-  // function finishLine(event){
- 
-  // }
+  function finishLine( finalscore ){
+    document.querySelector('#scorePage').classList.add('d-none')
+    document.querySelector(`#${finalscore}`).classList.remove('d-none')
+  }
   
